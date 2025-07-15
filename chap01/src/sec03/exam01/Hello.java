c565b9a698a7f1207e9035a5d2d2fa347d473258
Kimgyumwon <rlaruadnjs3@naver.com>
2025-07-15 10:13:49 +0900

    초기 프로젝트 설정

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,11 @@
+package sec03.exam01; //패키지 선언문
+// 주석 사용하기
+public class Hello { //클래스 선언 : 클래스 이름은 대문자로 시작하고 소스 파일 이름이랑 동일해야함
+
+	public static void main(String[] args) { //메인 메소드 : 자바 프로그램 실행 시 가장 먼저 실행되는 메소드
+		
+		System.out.println("Hello, Java"); // Hello, Java 를 출력하는 메소드
+		
+		}
+
+}
