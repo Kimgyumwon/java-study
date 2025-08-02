0bb08c8ff42e8d376904377cb7dae248381fcc80
Kimgyumwon <rlaruadnjs3@naver.com>
2025-08-02 13:16:23 +0900

    up

--- chap11/src/Homework/Homework02/Practice.java
+++ chap11/src/Homework/Homework02/Practice.java
@@ -2,5 +2,17 @@ package Homework.Homework02;
 
 public class Practice {
 	
-	
+	public String takeState(String data) {
+		// 문자열을 잘라서 배열에 넣음
+		String[]	 split = data.split(" ");
+		
+		// 문자열 마지막에 "구"가 있는지 확인
+		for(String word : split) {
+			if ( word.indexOf("구") == word.length()-1 && word.indexOf("구") != -1) {return word;}
+		}
+		
+		return null;
+		
+		
+	}
 }
--- chap11/src/Homework/Homework02/Run.java
+++ chap11/src/Homework/Homework02/Run.java
@@ -2,6 +2,16 @@ package Homework.Homework02;
 
 public class Run {
 	public static void main(String[] args) {
+		String[] data = new String[4];
+		data[0] = "서울특별시 금천구 구로디지털2로 95";
+		data[1] = "서울특별시 서대문구 연희로 248";
+		data[2] = "울산광역시 남구 돋질로 233";
+		data[3] = "경상북도 구미시 송정대로 55";
+		Practice pra = new Practice();
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(pra.takeState(data[i]));
+		}
+		
 		
 		
 		
